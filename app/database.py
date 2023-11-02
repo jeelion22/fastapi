bc019c98fc87501ab82c85609d68b04486f8e18a
jeeva m <jeelion22@gmail.com>
2023-11-02 13:09:41 +0530

    change SQLALCHEMY_DATABASE_URL

--- app/database.py
+++ app/database.py
@@ -1,13 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import sessionmaker, Session
-import os
 
-db_username = os.getenv("DB_USER")
-db_password = os.getenv("DB_PWD")
+# import os
+
+# db_username = os.getenv("DB_USER")
+# db_password = os.getenv("DB_PWD")
+from .config import settings
 
 SQLALCHEMY_DATABASE_URL = (
-    f"postgresql+psycopg2://{db_username}:{db_password}@localhost/fastapi"
+    f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}",
 )
 
 engine = create_engine(url=SQLALCHEMY_DATABASE_URL, echo=False)
