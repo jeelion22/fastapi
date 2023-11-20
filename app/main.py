201f39e0c2d23ba51a69172f4cbc0cc7616ac188
jeeva m <jeelion22@gmail.com>
2023-11-20 08:34:22 +0530

    change urls

--- app/main.py
+++ app/main.py
@@ -33,5 +33,11 @@ def root():
         "app_name": "Posts-APIs",
         "message": "Welcome to Posts-APIs! This is the home page for our sample API endpoints built with FastAPI.",
         "description": "Our API allows you to create, read, modify, delete, and vote on posts. For detailed documentation, please visit our API documentation page.",
-        "api_doc_url": "https://fastapi-posts-endpoints-jeeva-ca5c7ab434e2.herokuapp.com/docs",
+        "urls": {
+            "on_heroku": "https://fastapi-posts-endpoints-jeeva-ca5c7ab434e2.herokuapp.com/docs",
+            "on_virtualmachine": {
+                "root": "https://www.fastapi-blog-jeeva.xyc",
+                "docs": "https://www.fastapi-blog-jeeva.xyz/docs/",
+            },
+        },
     }
